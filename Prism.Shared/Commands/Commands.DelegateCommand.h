87fe6c2e168f31f26bb3a53c84129a6be6e8780f
Johan Lindfors <johan.lindfors@coderox.se>
2019-02-24 18:53:59 +0100

    Move code into shared project

---
+++ Prism.Shared/Commands/Commands.DelegateCommand.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "Commands.DelegateCommand.g.h"
+
+namespace winrt::Prism::Commands::implementation
+{
+	struct DelegateCommand : DelegateCommandT<DelegateCommand>
+	{
+		DelegateCommand() = delete;
+		DelegateCommand(Prism::Commands::ExecuteDelegate const& executeDelegate);
+		DelegateCommand(Prism::Commands::ExecuteDelegate const& executeDelegate, Prism::Commands::CanExecuteDelegate const& canExecuteDelegate);
+
+		void RaiseCanExecuteChanged();
+		winrt::event_token CanExecuteChanged(Windows::Foundation::EventHandler<Windows::Foundation::IInspectable> const& handler);
+		void CanExecuteChanged(winrt::event_token const& token) noexcept;
+		bool CanExecute(Windows::Foundation::IInspectable const& parameter);
+		void Execute(Windows::Foundation::IInspectable const& parameter);
+
+		void Execute();
+		bool CanExecute();
+
+	private:
+		event<Windows::Foundation::EventHandler<Windows::Foundation::IInspectable>> _canExecuteChanged;
+		Prism::Commands::ExecuteDelegate _executeDelegate;
+		Prism::Commands::CanExecuteDelegate  _canExecuteDelegate;
+		bool _lastCanExecute = true;
+	};
+}
+
+namespace winrt::Prism::Commands::factory_implementation
+{
+	struct DelegateCommand : DelegateCommandT<DelegateCommand, implementation::DelegateCommand>
+	{
+	};
+}
